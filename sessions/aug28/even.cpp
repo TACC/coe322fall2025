eb31d2941db6f8438d49ae843e65d95a2f11c3c3
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2025-08-28 21:09:35 -0500

    demo session aug 28

---
+++ sessions/aug28/even.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using std::cin,std::cout;
+// alternatively, but not as good:
+// using namespace std;
+
+int main() {
+  int i;
+  cin >> i;
+  cout << "That was: " << i << '\n';
+  // alternatively, use `endl' but `\n' preferred.
+
+  return 0;
+}
+
+// Emacs notes:
+
+// C-x C-c : exit
+// C-x C-s : save, then C-z suspend, fg : foreground
+
+// C-x 3 : split horizontally
+// C-x o : go to other pane
