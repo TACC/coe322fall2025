98fc83f6b69ce6a93bf0e4b5a9a30e561f54ee40
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2025-09-02 16:45:16 -0500

    collatz code

---
+++ sessions/sep02/collatz.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using std::cout,std::cin;
+
+int main() {
+  int starting_value;
+  cin >> starting_value;
+  int iteration_length=0;
+  for ( int current_value=starting_value; current_value!=1; ++iteration_length ) {
+    if (current_value%2==0)
+      // it's even, divide by 2
+      current_value /= 2;
+    else
+      // it's odd
+      current_value = 3*current_value+1;
+    // cout << current_value << '\n';
+  }
+  cout << "STarting at " << starting_value << " it took " << iteration_length << " iterations\n";
+
+  return 0;
+}
+
+// C-p : previous line
+// C-n : next line
+
+// C-k : `kill' to end of line (next, kill end of line)
+// C-y : `yank back' : paste
---
+++ sessions/sep02/loopfloat.cxx
@@ -0,0 +1,8 @@
+#include <iostream>
+using std::cout;
+
+int main() {
+  for (float x=0; x<=10.0; x+=.1)
+    cout << x << '\n';
+  return 0;
+}
