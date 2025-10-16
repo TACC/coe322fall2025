0427ee29e4750357ebf254c3a101f97a2b72b4b3
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2025-10-16 18:13:06 -0500

    cbuild now has options

--- sessions/cbuild.sh
+++ sessions/cbuild.sh
@@ -1,15 +1,42 @@
 #!/bin/bash
 
-if [ $# -ne 1 ] ; then
-    echo "Usage: $0 hwdir"
-    exit 0
+# if no arguments: point out usage
+if [ $# -eq 0 ] ; then
+	echo "Usage: cbuild.sh [ -h ] [ -m ] dir"
+	exit 0
 fi
-if [ ! -d $1 ] ; then
+
+# parse commandline arguments
+onlymake=0
+while [ $# -gt 1 ] ; do
+    if [ $1 = "-h" ] ; then
+	echo "Usage: cbuild.sh [ -h ] [ -m ] dir"
+	exit 0
+    elif [ $1 = "-m" ] ; then
+	onlymake=1
+	shift
+    else
+	echo "Error: unrecognized option <<$1>>"
+	exit 1
+    fi
+done
+
+# the remaining argument is the directory; test that it exists
+dir=$1
+if [ ! -d $dir ] ; then
     echo "Directory $1 does not exist"
     exit 1
 fi
-rm -rf build
-mkdir -p build
-cd build
-cmake ../$1
-make
+
+# now do the full cmake/make cycle
+if [ $onlymake -eq 0 ] ; then
+    rm -rf build
+    mkdir -p build
+    cd build
+    cmake ../$1
+    make
+else
+    # or only make
+    cd build
+    make
+fi
--- sessions/oct14/func.cpp
+++ sessions/oct14/func.cpp
@@ -2,4 +2,4 @@
 using std::cout;
 #include "func.hpp"
 
-void F::print() { cout << "Hello\n"; };
+void F::print() { cout << "Hello cruel world\n"; };
