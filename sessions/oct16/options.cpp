4dc5d68f91378def32a494bbe93f445e1b0e57b5
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2025-10-16 16:27:36 -0500

    options demo extended

--- sessions/oct16/options.cpp
+++ sessions/oct16/options.cpp
@@ -1,8 +1,23 @@
+#include <iostream>
+using std::cout;
 #include "cxxopts.hpp"
 
 int main( int argc,char** argv ) {
   cxxopts::Options options
     ("cxxopts","Commandline options demo");
+  options.add_options()("h,help","Usage information");
+  options.add_options()
+    ("n,ntimes","number of times",cxxopts::value<int>()->default_value("37"));
+
+  auto result = options.parse(argc,argv);
+  if ( result.count("h") ) {
+    cout << options.help() << '\n';
+  }
+  int ntimes = 0;
+  if ( result.count("n") )
+    ntimes = result["ntimes"].as<int>();
+  cout << ntimes << '\n';
+
   return 0;
 }
 
